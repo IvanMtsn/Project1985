3c44dd907f4225a879867ac289671d3e7440d31a
IvanMtsn <ivox300@gmail.com>
2026-02-05 19:32:01 +0100

    landing sway

--- Assets/scripts/Player/WalkBobAndSway.cs
+++ Assets/scripts/Player/WalkBobAndSway.cs
@@ -6,14 +6,17 @@ public class WalkBobAndSway : MonoBehaviour
 {
     Animator _animator;
     PlayerMovementV2 _pMov;
+    CameraController _camController;
     Vector3 _move;
-    Vector3 _airMove;
     [SerializeField] Transform _wSway;
     Vector3 _ogPosition;
+    bool _wasGrounded;
+    float _landingForce;
     void Start()
     {
         _animator = GetComponent<Animator>();
         _pMov = GetComponentInParent<PlayerMovementV2>();
+        _camController = GetComponentInParent<CameraController>();
         _ogPosition = _wSway.localPosition;
     }
     void Update()
@@ -26,19 +29,24 @@ public class WalkBobAndSway : MonoBehaviour
         _animator.SetFloat("velZ", _move.y,0.05f, Time.deltaTime);
         //weapon sway stuff:
         //Debug.Log(_pMov.Rb.linearVelocity.magnitude);
-        if (!_pMov.GroundCheck.IsGrounded)
+        //Debug.Log(_pMov.Rb.linearVelocity.y/10);
+        if (!_pMov.GroundCheck.IsGrounded || (_pMov.GroundCheck.IsGrounded && _pMov.IsDashing))
         {
             Vector3 leck = transform.InverseTransformDirection(_pMov.Rb.linearVelocity);
-            _airMove = new Vector3(-leck.x / 140, -leck.y / 110, -leck.z / 75);
-            _wSway.localPosition = Vector3.Lerp(_wSway.localPosition, _airMove, Time.deltaTime * 10f);
+            Vector3 sway = new Vector3(-leck.x / 140, -leck.y / 110, -leck.z / 75);
+            _wSway.localPosition = Vector3.Lerp(_wSway.localPosition, sway, Time.deltaTime * 12f);
         }
-        else if (_pMov.GroundCheck.IsGrounded && Vector3.Distance(_wSway.localPosition, _ogPosition) >= 0.005f)
+        else if (_pMov.GroundCheck.IsGrounded)
         {
-            _wSway.localPosition = Vector3.MoveTowards(_wSway.localPosition, _ogPosition, Time.deltaTime );
+            _landingForce = Mathf.Lerp(_landingForce,0,Time.deltaTime * 22);
+            Vector3 swayTarget = _ogPosition + Vector3.down * _landingForce;
+            _wSway.localPosition = Vector3.Lerp(_wSway.localPosition, swayTarget, Time.deltaTime * 4);
+            _camController.ShakeCamera(_landingForce * 2);
         }
-    }
-    void Land()
-    {
-
+        _wasGrounded = _pMov.GroundCheck.IsGrounded;
+        if (!_wasGrounded)
+        {
+            _landingForce = Mathf.Min(Mathf.Abs(_pMov.Rb.linearVelocity.y) / 10, 3f);
+        }
     }
 }
